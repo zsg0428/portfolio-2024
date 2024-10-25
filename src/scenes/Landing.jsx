88d9d12ce03e4fee5aeb69d1764cc0768551bf43
zsg0428 <zsg258852@gmail.com>
2024-10-24 23:02:01 -0400

    will be continue to work on the landing page

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+function Landing({ setSelectedPage }) {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
+    >
+      {/* IMAGE SECTION */}
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+            <img
+              src="/public"
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+function Navbar({ isTopOfPage, selectedPage, setSelectedPage }) {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? " " : "bg-red";
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6 ${navbarBackground}`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfiar text-3xl font-bold">SZ</h4>
+
+        {/* Desktop NAV */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              setSelectedPage={setSelectedPage}
+              selectedPage={selectedPage}
+            />
+            <Link
+              page="Skills"
+              setSelectedPage={setSelectedPage}
+              selectedPage={selectedPage}
+            />
+            <Link
+              page="Projects"
+              setSelectedPage={setSelectedPage}
+              selectedPage={selectedPage}
+            />
+            <Link
+              page="Testimonials"
+              setSelectedPage={setSelectedPage}
+              selectedPage={selectedPage}
+            />
+            <Link
+              page="Contact"
+              setSelectedPage={setSelectedPage}
+              selectedPage={selectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled((prev) => !prev)}
+          >
+            <MenuIcon />
+          </button>
+        )}
+
+        {/* Mobile menu popup */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <CloseIcon />
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue ">
+              <Link
+                page="Home"
+                setSelectedPage={setSelectedPage}
+                selectedPage={selectedPage}
+              />
+              <Link
+                page="Skills"
+                setSelectedPage={setSelectedPage}
+                selectedPage={selectedPage}
+              />
+              <Link
+                page="Projects"
+                setSelectedPage={setSelectedPage}
+                selectedPage={selectedPage}
+              />
+              <Link
+                page="Testimonials"
+                setSelectedPage={setSelectedPage}
+                selectedPage={selectedPage}
+              />
+              <Link
+                page="Contact"
+                setSelectedPage={setSelectedPage}
+                selectedPage={selectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
